c030ceb2fc8e14df4bcf9129e108744947a59a25
radmuffin <70724879+radmuffin@users.noreply.github.com>
2025-02-06 10:04:45 -0700

    added css

--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import Peer, { DataConnection } from "peerjs";
+import './styles.css';
 
 const iceConfig = {
     iceServers: [
