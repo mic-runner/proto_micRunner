19bdc7250323a1e2df96319e97a80cf221ffba8d
radmuffin <70724879+radmuffin@users.noreply.github.com>
2025-03-14 10:27:51 -0600

    added turn credentials

--- index.tsx
+++ index.tsx
@@ -7,9 +7,27 @@ const iceConfig = {
   iceServers: [
     { urls: "stun:stun.l.google.com:19302" }, // Primary STUN
     {
-      urls: "turn:openrelay.metered.ca:443", // Primary TURN
-      username: "openrelayproject",
-      credential: "openrelayproject",
+      urls: "stun:stun.relay.metered.ca:80", // Other STUN
+    },
+    {
+      urls: "turn:global.relay.metered.ca:80", //TURNs with credentials
+      username: "e3ad760fe887d9e08a94e18f",
+      credential: "S4dcWSoVsJ/CSU1x",
+    },
+    {
+      urls: "turn:global.relay.metered.ca:80?transport=tcp",
+      username: "e3ad760fe887d9e08a94e18f",
+      credential: "S4dcWSoVsJ/CSU1x",
+    },
+    {
+      urls: "turn:global.relay.metered.ca:443",
+      username: "e3ad760fe887d9e08a94e18f",
+      credential: "S4dcWSoVsJ/CSU1x",
+    },
+    {
+      urls: "turns:global.relay.metered.ca:443?transport=tcp",
+      username: "e3ad760fe887d9e08a94e18f",
+      credential: "S4dcWSoVsJ/CSU1x",
     },
   ],
 };
